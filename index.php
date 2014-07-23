4aaa9a68b74ff4c138250a69f0262bf348986484
php developer <fingercrew2@yandex.ru>
2014-07-23 05:15:26 +0300

    xada. index.php was fixed

--- index.php
+++ index.php
@@ -1,11 +1,4 @@
 <?php
-//3
-//4
-//5
-//6
-//7
-//8
-phpinfo();
 ini_set('session.name',             'mam');
 ini_set('session.use_cookies',      true);
 ini_set('session.use_trans_sid',    false);
